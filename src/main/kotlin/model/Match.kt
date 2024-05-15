d42fe122d6927743abff889c37cb9dffbf7fd4aa
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2024-05-15 15:36:09 +0200

    Create Match

---
+++ src/main/kotlin/model/Match.kt
@@ -0,0 +1,137 @@
+package model
+
+import utils.Math
+import kotlin.random.Random
+
+class Match(val players: MutableList<Player>, val deck: MutableList<Card>) {
+    private var playingFirstPlayer = true
+    private var winner: Player? = null
+    private var lastCard: Card? = null
+    private var briscolaSuit: Suit? = null
+    private val playedCards = mutableListOf<Card>()
+
+    /**
+     * Returns true if the first player is playing, false otherwise
+     * @return true if the first player is playing, false otherwise
+     */
+    fun playingFirstPlayer(): Boolean {
+        return playingFirstPlayer
+    }
+
+    /**
+     * Get the winner of the match
+     * @return the winner of the match
+     */
+    fun getWinner(): Player? {
+        return winner
+    }
+
+    /**
+     * Get the last card drawn from the deck
+     * @return the last card drawn from the deck
+     */
+    fun getLastCard(): Card? {
+        return lastCard
+    }
+
+    /**
+     * Get the briscola suit
+     */
+    fun getBriscolaSuit(): Suit? {
+        return briscolaSuit
+    }
+
+    /**
+     * Get the cards played in the match
+     */
+    fun getPlayedCards(): List<Card> {
+        return playedCards
+    }
+
+    /**
+     * Prepare the match by shuffling the deck, setting the last card and giving 3 cards to each player
+     */
+    fun prepareMatch() {
+        deck.shuffle()
+
+        lastCard = deck.removeAt(0)
+        briscolaSuit = lastCard!!.getSuit()
+
+        if (Random.nextBoolean()) {
+            switchTurn()
+        }
+
+        for (i in 0 until 3) {
+            playerDrawCard(players[0])
+            playerDrawCard(players[1])
+        }
+    }
+
+    fun playerPlayCard(player: Player, card: Card) {
+        if (!player.hasCardInHand(card)) {
+            return
+        }
+        if (playingFirstPlayer) {
+            cardPlayedFirst(player, card)
+            playingFirstPlayer = false
+        } else {
+            cardPlayedSecond(player, card)
+            playingFirstPlayer = true
+        }
+    }
+
+    private fun switchTurn() {
+        players.reverse()
+    }
+
+    private fun playerDrawCard(player: Player) {
+        if (deck.isEmpty()) {
+            if (lastCard != null) {
+                player.drawCard(lastCard!!)
+                lastCard = null
+            }
+            return
+        }
+        player.drawCard(deck.removeAt(0))
+    }
+
+    private fun cardPlayedFirst(player: Player, card: Card) {
+        player.playCard(card)
+        playedCards.add(card)
+    }
+
+    private fun cardPlayedSecond(player: Player, card: Card) {
+        player.playCard(card)
+        playedCards.add(card)
+        val higherCard = Math.getHigherCard(playedCards[0], playedCards[1], briscolaSuit!!)
+        if (higherCard == playedCards[1]) {
+            switchTurn()
+        }
+
+        playerDrawCard(players[0])
+        playerDrawCard(players[1])
+
+        players[0].gainCard(playedCards[0])
+        players[0].gainCard(playedCards[1])
+
+        playedCards.clear()
+
+        try {
+            checkWinner()
+        } catch (e: IllegalStateException) {
+            println(e.message)
+        }
+    }
+
+    private fun checkWinner() {
+        if (players[0].getHandCards().isEmpty() && players[1].getHandCards().isEmpty()) {
+            winner = if (players[0].points() > players[1].points()) {
+                players[0]
+            } else if (players[0].points() < players[1].points()) {
+                players[1]
+            } else {
+                throw IllegalStateException("Match ended in a draw")
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/model/TestMatch.kt
@@ -0,0 +1,80 @@
+package model
+
+import utils.Math
+import kotlin.test.*
+
+class TestMatch {
+    private val player1 = Player("Player 1")
+    private val player2 = Player("Player 2")
+    private val match = Match(mutableListOf(player1, player2), Card.entries.toMutableList())
+
+    @Test
+    fun testInit() {
+        assertEquals(player1, match.players[0])
+        assertEquals(player2, match.players[1])
+        assertEquals(40, match.deck.size)
+        assertEquals(0, match.getPlayedCards().size)
+        assertNull(match.getWinner())
+        assertNull(match.getLastCard())
+        assertNull(match.getBriscolaSuit())
+    }
+
+    @Test
+    fun testPlayingFirstPlayer() {
+        assertTrue(match.playingFirstPlayer())
+    }
+
+    @Test
+    fun testGetWinner() {
+        assertNull(match.getWinner())
+    }
+
+    @Test
+    fun testGetLastCard() {
+        assertNull(match.getLastCard())
+    }
+
+    @Test
+    fun testGetBriscolaSuit() {
+        assertNull(match.getBriscolaSuit())
+    }
+
+    @Test
+    fun testGetPlayedCards() {
+        assertEquals(0, match.getPlayedCards().size)
+    }
+
+    @Test
+    fun testPrepareMatch() {
+        match.prepareMatch()
+        assertEquals(33, match.deck.size)
+        assertEquals(0, match.getPlayedCards().size)
+        assertNotNull(match.getLastCard())
+        assertNotNull(match.getBriscolaSuit())
+        assertEquals(3, player1.getHandCards().size)
+        assertEquals(3, player2.getHandCards().size)
+    }
+
+    @Test
+    fun testPlayerPlayCard() {
+        match.prepareMatch()
+
+        val card1 = match.players[0].getHandCards()[0]
+        match.playerPlayCard(match.players[0], card1)
+        assertEquals(card1, match.getPlayedCards()[0])
+
+        val card2 = match.players[1].getHandCards()[0]
+        match.playerPlayCard(match.players[1], card2)
+        println(card1)
+        println(card2)
+        println(match.getPlayedCards())
+        println(match.players[0].getGainedCards())
+        println(match.players[1].getGainedCards())
+        if (Math.getHigherCard(card1, card2, match.getBriscolaSuit()!!) == card1) {
+            assertTrue(match.players[0].getGainedCards().isNotEmpty())
+            assertTrue(match.players[1].getGainedCards().isEmpty())
+        }
+        assertEquals(3, match.players[0].getHandCards().size)
+        assertEquals(3, match.players[1].getHandCards().size)
+    }
+}
