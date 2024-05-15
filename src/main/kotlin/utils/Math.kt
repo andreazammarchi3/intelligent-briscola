27fed1d026c00344ede8fb38b9888e10b66483b7
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2024-05-15 14:57:05 +0200

    Create Math Utils

---
+++ src/main/kotlin/utils/Math.kt
@@ -0,0 +1,46 @@
+package utils
+
+import model.Card
+import model.Suit
+
+/**
+ * Utility class for math operations.
+ */
+class Math {
+    companion object {
+        /**
+         * Returns the higher card between two cards.
+         * If a card is the briscola, it is considered higher than any other card.
+         * If the two cards have different suits, the card with the first suit is returned.
+         * If the two cards have the same suit, the card with the highest value is returned.
+         * If the two cards have the same value, the card with the highest rank is returned.
+         * @param card1 the first card
+         * @param card2 the second card
+         * @param briscolaSuit the briscola suit
+         * @return the higher card
+         */
+        fun getHigherCard(card1: Card, card2: Card, briscolaSuit: Suit): Card {
+            if (card1.getSuit() == briscolaSuit && card2.getSuit() != briscolaSuit) {
+                return card1
+            } else if (card1.getSuit() != briscolaSuit && card2.getSuit() == briscolaSuit) {
+                return card2
+            } else {
+                return if (card1.getSuit() != card2.getSuit()) {
+                    card1
+                } else {
+                    if (card1.getValue() < card2.getValue()) {
+                        card2
+                    } else if (card1.getValue() > card2.getValue()) {
+                        card1
+                    } else {
+                        if (card1.getRank() < card2.getRank()) {
+                            card2
+                        } else {
+                            card1
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/test/kotlin/utils/TestMath.kt
@@ -0,0 +1,32 @@
+package utils
+
+import model.Card
+import model.Suit
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class TestMath {
+    @Test
+    fun testGetHigherCard() {
+        // Test same suit
+        val card1 = Card.CLUBS_1
+        val card2 = Card.CLUBS_2
+        assertEquals(card1, Math.getHigherCard(card1, card2, Suit.COINS))
+
+        // Test same value
+        val card3 = Card.CLUBS_2
+        assertEquals(card2, Math.getHigherCard(card2, card3, Suit.COINS))
+
+        // Test same rank
+        val card4 = Card.COINS_2
+        assertEquals(card4, Math.getHigherCard(card2, card4, Suit.COINS))
+
+        // Test briscola
+        val card5 = Card.COINS_3
+        assertEquals(card5, Math.getHigherCard(card4, card5, Suit.COINS))
+
+        // Test briscola and different suit
+        val card6 = Card.CLUBS_3
+        assertEquals(card5, Math.getHigherCard(card6, card5, Suit.COINS))
+    }
+}
