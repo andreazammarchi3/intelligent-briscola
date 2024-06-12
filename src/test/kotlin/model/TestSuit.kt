4d47f5a8f3d251266104d4088539698ad0dd2190
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2024-06-12 18:43:55 +0200

    Fix some tests

--- src/test/kotlin/model/TestSuit.kt
+++ src/test/kotlin/model/TestSuit.kt
@@ -7,9 +7,9 @@ import kotlin.test.assertEquals
 class TestSuit {
     @Test
     fun testToString() {
-        assertEquals("Clubs", Suit.CLUBS.toString())
-        assertEquals("Coins", Suit.COINS.toString())
-        assertEquals("Cups", Suit.CUPS.toString())
-        assertEquals("Swords", Suit.SWORDS.toString())
+        assertEquals("clubs", Suit.CLUBS.toString())
+        assertEquals("coins", Suit.COINS.toString())
+        assertEquals("cups", Suit.CUPS.toString())
+        assertEquals("swords", Suit.SWORDS.toString())
     }
 }
