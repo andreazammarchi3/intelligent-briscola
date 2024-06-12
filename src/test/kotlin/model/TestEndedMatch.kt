dfbb45aa2a17a17d299ccef4dd73d5e555b87e37
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2024-06-12 18:33:44 +0200

    Fix some tests

--- src/test/kotlin/model/TestEndedMatch.kt
+++ src/test/kotlin/model/TestEndedMatch.kt
@@ -7,7 +7,7 @@ import kotlin.test.assertEquals
 class TestEndedMatch {
     @Test
     fun testEndedMatch() {
-        val endedMatch = EndedMatch("player1", "Win", 61, 59)
+        val endedMatch = EndedMatch("player1", "stupid", "Win", 61, 59)
         assertEquals("player1", endedMatch.playerName)
         assertEquals("Win", endedMatch.result)
         assertEquals(61, endedMatch.playerPoints)
@@ -16,8 +16,8 @@ class TestEndedMatch {
 
     @Test
     fun testEquals() {
-        val endedMatch1 = EndedMatch("player1", "Win", 61, 59)
-        val endedMatch2 = EndedMatch("player1", "Win", 61, 59)
+        val endedMatch1 = EndedMatch("player1", "stupid", "Win", 61, 59)
+        val endedMatch2 = EndedMatch("player1", "stupid", "Win", 61, 59)
         assertEquals(endedMatch1, endedMatch2)
     }
 }
